f98897d57d784d097b32bee24de5d60178e2ca64
Parhuam1 <44383266+Parhuam1@users.noreply.github.com>
2018-11-30 20:09:03 -0800

    Create Options.cpp

---
+++ Menus/Menus/Options.cpp
@@ -0,0 +1,53 @@
+#include "Options.h"
+//#include "FoodFileManager.h"
+#include "FoodCreator.h"
+#include "FileIO.h"
+#include "Menu.h"
+#include "Check.h"
+
+#include <iostream>
+using namespace std;
+
+Menu menu_1;
+
+
+void Options::newData() 
+{
+	menu_1.addNewDataMenu();
+
+}
+
+void Options::deleteData()
+{
+
+}
+
+void Options::findData()
+{
+
+}
+
+void Options::listDataHash()
+{
+
+}
+
+void Options::listDataSequence()
+{
+
+}
+
+void Options::efficiency()
+{
+
+}
+
+void Options::printIndentedTree()
+{
+
+}
+
+void Options::teamChoice()
+{
+
+}
