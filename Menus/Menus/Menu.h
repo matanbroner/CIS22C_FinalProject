fc27d81e3cdf4d9259b51356caa557034e6e665f
Parhuam1 <44383266+Parhuam1@users.noreply.github.com>
2018-11-30 20:04:07 -0800

    Update Menu.h

--- Menus/Menus/Menu.h
+++ Menus/Menus/Menu.h
@@ -1,4 +1,7 @@
 #pragma once
+#ifndef MENU_H
+#define MENU_H
+
 const int PROMPT_SIZE = 51;			// MAXIMUM STRING SIZE
 
 class Menu
@@ -6,10 +9,19 @@ class Menu
 private:
 	char input[PROMPT_SIZE];		// User's input
 public:
-	Menu();						    // Constructor
-	void mainMenu();				// Main menu output
-	char* itemListPrompt();			// Set input
-	char* simplePrompt();			// Set input
-};
+	Menu();				// Constructor
+	void mainMenu();		// Main menu output
+	void addNewDataMenu();          // Add new Data Menu
+	void deleteDataMenu();		// Delete Data Menu
+	void findAndDisplayMenu();      // Find and display Menu
+	void listDataPrint();           // Print data in hash table sequence
+	void printTree();		// Print Indented tree
+	void pIT();
+	void printEfficieny();          // Print Efficiency
+	void printTeamChoices();
 
+	char* itemListPrompt();		// Set input
+	char* simplePrompt();		// Set input
+};
+#endif 
 
