31a83b148c5c30d22b17e3c063108429c638cd28
Parhuam1 <44383266+Parhuam1@users.noreply.github.com>
2018-11-30 20:08:07 -0800

    Create Options.h

---
+++ Menus/Menus/Options.h
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef OPTIONS_H
+#define OPTIONS_H
+
+class Options
+{
+private:
+public:
+	void newData();
+	void deleteData();
+	void findData();
+	void listDataHash();
+	void listDataSequence();
+	void printIndentedTree();
+	void efficiency();
+	void teamChoice();
+
+
+
+};
+#endif 
